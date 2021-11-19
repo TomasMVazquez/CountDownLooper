eef98ec07b5d4219340fe12f4fc33988aee59831
Toms <tomas.manuel.vazquez@gmail.com>
2021-11-19 09:11:49 +0100

    Portrait + delete all unnecessary code

--- app/src/main/java/com/toms/applications/countdownlooper/screens/init/TimerInitFragment.kt
+++ app/src/main/java/com/toms/applications/countdownlooper/screens/init/TimerInitFragment.kt
@@ -2,41 +2,27 @@ package com.toms.applications.countdownlooper.screens.init
 
 import android.os.Bundle
 import android.view.*
-import android.view.inputmethod.EditorInfo
-import android.widget.NumberPicker.OnScrollListener.SCROLL_STATE_IDLE
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.toms.applications.countdownlooper.R
-import com.toms.applications.countdownlooper.databinding.FragmentTimerInitBinding
-import com.toms.applications.countdownlooper.utils.hideKeyboard
 import androidx.navigation.fragment.findNavController
-import com.google.accompanist.flowlayout.FlowColumn
 import com.toms.applications.countdownlooper.ui.components.*
 import com.toms.applications.countdownlooper.utils.composeView
 import com.toms.applications.countdownlooper.utils.toTimeFormat
@@ -46,8 +32,6 @@ import com.toms.applications.countdownlooper.utils.toTimer
 @ExperimentalComposeUiApi
 @ExperimentalFoundationApi
 class TimerInitFragment : Fragment() {
-
-//    private lateinit var binding: FragmentTimerInitBinding
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -141,55 +125,6 @@ class TimerInitFragment : Fragment() {
             }
         }
 
-        /*binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_timer_init,
-                container,
-                false
-        )
-
-        setHasOptionsMenu(true)
-
-        with(binding){
-            reRunTIET.setText("0")
-
-            with(numPickerHours){
-                maxValue = 23
-                setFormatter { String.format("%02d", it) }
-                setOnScrollListener { picker, scrollState -> if(scrollState == SCROLL_STATE_IDLE) { onTimeSettled() } }
-            }
-            with(numPickerMinutes){
-                maxValue = 59
-                setFormatter { String.format("%02d", it) }
-                setOnScrollListener { picker, scrollState -> if(scrollState == SCROLL_STATE_IDLE) { onTimeSettled() } }
-            }
-            with(numPickerSeconds){
-                maxValue = 59
-                setFormatter { String.format("%02d", it) }
-                setOnScrollListener { picker, scrollState -> if(scrollState == SCROLL_STATE_IDLE) { onTimeSettled() } }
-            }
-
-            reRunTIET.setOnEditorActionListener { v, actionId, event ->
-                if (actionId == EditorInfo.IME_ACTION_DONE){
-                    if(v.text.isNullOrEmpty()) v.text = "0"
-                    v.clearFocus()
-                    hideKeyboard()
-                }
-                return@setOnEditorActionListener true
-            }
-
-            startFAB.setOnClickListener {
-                val action = TimerInitFragmentDirections.actionTimerSettingsFragmentToTimerFragment(
-                    numPickerHours.value,
-                    numPickerMinutes.value,
-                    numPickerSeconds.value,
-                    reRunTIET.text.toString().toInt()
-                )
-                findNavController().navigate(action)
-            }
-        }
-
-        return binding.root*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -214,17 +149,6 @@ class TimerInitFragment : Fragment() {
             repetition = repetitions
         )
         findNavController().navigate(action)
-    }
-
-
-    private fun onTimeSettled() {
-       /* with(binding){
-            if (numPickerHours.value > 0 || numPickerMinutes.value > 0 || numPickerSeconds.value > 0){
-                startFAB.visibility = View.VISIBLE
-            }else{
-                startFAB.visibility = View.GONE
-            }
-        }*/
     }
 
 }
